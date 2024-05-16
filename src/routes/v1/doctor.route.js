8a34360a67586fe9b8e72d8a02fc5d5aa874cb91
Andyana Lilmuttaqina Mafaza <andyanamafaza@gmail.com>
2024-05-16 22:35:11 +0700

    feat: add validation and auth in doctor endpoint

--- src/routes/v1/doctor.route.js
+++ src/routes/v1/doctor.route.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const auth = require('../../middlewares/auth');
+const validate = require('../../middlewares/validate');
 const doctorController = require('../../controllers/doctor.controller');
+const doctorValidation = require('../../validations/doctor.validation');
 
 const router = express.Router();
 
-router.get('/', auth('admin'), doctorController.queryDoctors);
+router.get('/', auth('admin'), validate(doctorValidation.getDoctors), doctorController.getDoctors);
 
 router.get('/:doctorId', auth('admin'), doctorController.getDoctorById);
 
-router.patch('/verify/:doctorId', doctorController.verifyDoctor);
+router.patch('/verify/:doctorId', auth('admin'), validate(doctorValidation.verifyDoctor), doctorController.verifyDoctor);
 
 module.exports = router;
