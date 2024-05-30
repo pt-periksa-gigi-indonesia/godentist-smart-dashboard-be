a1ed26d1ead819d7ff773738548e88325efe11ec
Andyana Lilmuttaqina Mafaza <andyanamafaza@gmail.com>
2024-05-30 11:02:33 +0700

    feat: seed endpoint modification

--- src/controllers/seed.controller.js
+++ src/controllers/seed.controller.js
@@ -1,12 +1,21 @@
-const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const seedDatabase = require('../config/seed');
+const { seedLog } = require('../models');
 
 const seed = catchAsync(async (req, res) => {
   await seedDatabase();
-  res.status(httpStatus.CREATED).send({ message: 'Database successfully seeded' });
+  const latestSeedLog = await seedLog.findOne().sort({ _id: -1 });
+  res
+    .status(latestSeedLog.status_code)
+    .send({ status_code: latestSeedLog.status_code, message: latestSeedLog.message, createdAt: latestSeedLog.createdAt });
+});
+
+const latestSeedDate = catchAsync(async (req, res) => {
+  const latestSeedLog = await seedLog.findOne().sort({ _id: -1 });
+  res.send({ createdAt: latestSeedLog.createdAt });
 });
 
 module.exports = {
   seed,
+  latestSeedDate,
 };
